178b909f7e143a01cb417b60d3cbe3058d413b87
Ana Sofia Vanegas <Anasofiaav1309@gmail.com>
2025-07-21 21:13:16 -0500

    correcciones

--- .github/Actividad1/investigacion/actividad4.asm
+++ .github/Actividad1/investigacion/actividad4.asm
@@ -2,3 +2,13 @@
 D = A
 @16
 M = D
+@50
+D=A
+@16
+M=D+M
+//Guardar un 100 en RAM[32]  
+
+@100 
+D=A 
+@32 = Cargar la dirección de RAM[32] en el registro A
+M=D = Guardar el valor de D (100) en RAM[32]
